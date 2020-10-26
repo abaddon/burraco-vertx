d36e6b58a12f9acacc7e7c8be5ebdc1f15ef77af
Stefano <34969+abaddon@users.noreply.github.com>
2020-10-26 08:41:45 +0000

    Update ExtendEventTest.kt
    added test

--- Engine/src/test/kotlin/com/abaddon83/vertx/burraco/engine/adapters/eventStoreAdapter/ExtendEventTest.kt
+++ Engine/src/test/kotlin/com/abaddon83/vertx/burraco/engine/adapters/eventStoreAdapter/ExtendEventTest.kt
@@ -5,6 +5,8 @@ import io.vertx.core.json.JsonObject
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.junit.Test
+import java.time.Instant
+import java.util.*
 import kotlin.test.assertEquals
 
 class ExtendEventTest {
@@ -19,5 +21,29 @@ class ExtendEventTest {
 //        println(json.getString("jsonPayload"))
         val event = ExtendEvent(json)
         assertEquals(event.name, "BurracoGameCreated")
+    }
+
+    @Test
+    fun `Given a json string I want to convert in the right Event model`(){
+        //Json.decodeFromString<ExtendEvent>()
+
+        val jsonString: String = "fun `Given a json string I want to convert in the ExtendedEvent model`(){\n" +
+                "        //Json.decodeFromString<ExtendEvent>()\n" +
+                "        val json: JsonObject = JsonObject(\"{\\\"name\\\":\\\"BurracoGameCreated\\\",\\\"entityKey\\\":\\\"ae2da3c3-48a8-4b2f-99c1-669fb52e700e\\\",\\\"entityName\\\":\\\"BurracoGame\\\",\\\"instant\\\":\\\"2020-10-20T08:38:53Z\\\",\\\"jsonPayload\\\":\\\"{}\\\"}\")\n" +
+                "//        println(json.getString(\"name\"))\n" +
+                "//        println(json.getString(\"entityKey\"))\n" +
+                "//        println(json.getInstant(\"instant\"))\n" +
+                "//        println(json.getString(\"jsonPayload\"))\n" +
+                "        val event = ExtendEvent(json)\n" +
+                "        assertEquals(event.name, \"BurracoGameCreated\")\n" +
+                "    }"
+
+//        println(json.getString("name"))
+//        println(json.getString("entityKey"))
+//        println(json.getInstant("instant"))
+//        println(json.getString("jsonPayload"))
+        val extendedEvent = ExtendEvent("", UUID.randomUUID(),"", Instant.now(),jsonString)
+        val event= extendedEvent.toEvent()
+
     }
 }
