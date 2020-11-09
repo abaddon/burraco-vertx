d076fc2c6a5b4e6ec190c63cd4dbe2494d5b56a4
Stefano <34969+abaddon@users.noreply.github.com>
2020-11-09 16:34:58 +0000

    added connector worker in the docker composer

--- KafkaCluster/buildImages.sh
+++ KafkaCluster/buildImages.sh
@@ -2,7 +2,9 @@
 
 docker build -t zookeeper-node -f image_zk_node/Dockerfile image_zk_node
 docker build -t kafka-node -f image_kafka_node/Dockerfile image_kafka_node
+docker build -t kafka-worker -f image_kafka_worker/Dockerfile image_kafka_worker
 docker build -t kafka -f image_kafka/Dockerfile image_kafka
+
 
 
 # docker build -t kafka-training -f image/Dockerfile image
---
+++ KafkaCluster/image_kafka_worker/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/var/kafka/bin/connect-distributed.sh /var/content/$KAFKA_WORKER_CONFIG
