1a35c7814e36acaa6e700729ad80bd104f18b32e
Stefano <34969+abaddon@users.noreply.github.com>
2022-07-19 09:17:46 +0100

    update kotlin plugin

--- Dealer/build.gradle.kts
+++ Dealer/build.gradle.kts
@@ -24,7 +24,7 @@ val watchForChange = "src/**/*"
 val doOnChange = "./gradlew classes"
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.palantir.git-version") version "0.15.0"
     jacoco
